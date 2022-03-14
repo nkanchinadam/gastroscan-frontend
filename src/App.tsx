f2ca3e47942e39e8e89eb07a202b55bde4222be8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-03-14 01:21:29 -0400

    changed App.tsx to consist of a basic file upload user interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './src/logo.svg';
 
 function App() {
+  const onFileUpload = (): void => {
+    console.log('File uploaded');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Upload Image or Video Here</label><br/>
+      <input type="file" accept="image/*, video/*" /><br/>
+      <button onClick={onFileUpload}>Upload</button>
     </div>
   );
 }
