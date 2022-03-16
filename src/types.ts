bc58820d51acc7e018eeed9809f041f0f344be61
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-03-16 12:57:50 -0400

    created types.ts to store different custom types

---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type ModelType = 'abnormality' | 'condition';
+export type AbnormalityLabels = 'Healthy' | 'Hemorrhoids' | 'Polyps';
+export type ConditionLabels = 'Barretts' | 'Esophagitis' | 'Healthy' | 'Hemorrhoids' | 'Polyps' | 'Ulcerative Colitis';
